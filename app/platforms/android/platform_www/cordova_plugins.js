cf42c9785e58798bb83ce139f543593addf52764
vHeemstra <philipvanheemstra@hotmail.com>
2016-02-27 20:38:28 +0100

    Init...

---
+++ app/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
